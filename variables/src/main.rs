83bf9c1f4a39a3e26b18b42473e1c1c4d7805c20
seaerchin <jer005@e.ntu.edu.sg>
2021-11-23 18:20:07 +0800

    feat(variables): add code

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("x: {}", x);
+    x = 7;
+    println!("x: {}", x);
+
+    let tup = (1, 2, 3);
+
+    let (a, b, c) = tup;
+    println!("destructuring: {}, {}, {}", a, b, c);
+
+    println!("test: {}", test(1234));
+
+    let y = {
+        let x = 3;
+        x + 1 // no semicolon - this is an expression and the return value of the {} block
+    };
+    println!("y: {}", y);
+
+    // arrays are immutable
+    let arr = [1, 2, 3, 4, 5];
+    // 2 loop constructs, while and for
+    let mut index = 0;
+    while index < 5 {
+        println!("arr item at index {}: {}", index, arr[index]);
+        index += 1;
+    }
+
+    // note the iter() call
+    for item in arr.iter() {
+        println!("item: {}", item);
+    }
+}
+
+fn test(x: i32) -> i32 {
+    x
 }
